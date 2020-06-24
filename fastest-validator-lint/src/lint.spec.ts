d1f57e19b283ad23c9e44b1f80bb070a6d62e95c
yannivt <yanivt@traxretail.com>
2020-06-24 10:46:03 +0300

    eslint

--- fastest-validator-lint/src/lint.spec.ts
+++ fastest-validator-lint/src/lint.spec.ts
@@ -1,7 +1,7 @@
 import { TSESLint } from '@typescript-eslint/experimental-utils';
 import * as parser from '@typescript-eslint/parser';
 import {rule} from './lint';
-
+export {}
 describe('Class validation decorators', () => {
   const linter = new TSESLint.Linter();
   linter.defineRule('array-type', rule);
@@ -10,36 +10,49 @@ describe('Class validation decorators', () => {
   it('validation decorators', () => {
     const code =
 `
-class SomeClass2{}
-class SomeClass {
-  someBoolProp: boolean;
-  someOptionalBollProp?: boolean;
-  nestedClass: SomeClass2;
-  someNumber: number;
-  someString: string;
-  someArrayOfString: string[];
-  someArrayOfObject: SomeClass2[];
-  someArrayOfObjectOptional?: SomeClass2[];
+class SomeClass2{
+    some: number;
 }
+export class Device{
+   pk: number;
+   location: string;
+   someBoolean?: boolean;
+   someBoolProp: boolean;
+   someOptionalBollProp?: boolean;
+   nestedClass: SomeClass2;
+   someNumber: number;
+   someString: string;
+   someArrayOfString: string[];
+   someArrayOfObject: SomeClass2[];
+   someArrayOfObjectOptional?: SomeClass2[];
+   constructor(device: Device) {
+  }
+}
+
 `
 
     const output =
       `
 @Schema()
-class SomeClass2{}
-@Schema()
-class SomeClass {
-  @Boolean() someBoolProp: boolean;
-  @Boolean({optional: true}) someOptionalBollProp?: boolean;
-  @Nested() nestedClass: SomeClass2;
-  @Number() someNumber: number;
-  @String() someString: string;
-  @Array() someArrayOfString: string[];
-  @NestedArray(SomeClass2) someArrayOfObject: SomeClass2[];
-  @NestedArray(SomeClass2, {optional: true}) someArrayOfObjectOptional?: SomeClass2[];
+class SomeClass2{
+    @Number() some: number;
 }
-
-
+@Schema()
+export class Device{
+   @Number() pk: number;
+   @String() location: string;
+   @Boolean({optional: true}) someBoolean?: boolean;
+   @Boolean() someBoolProp: boolean;
+   @Boolean({optional: true}) someOptionalBollProp?: boolean;
+   @Nested() nestedClass: SomeClass2;
+   @Number() someNumber: number;
+   @String() someString: string;
+   @Array() someArrayOfString: string[];
+   @NestedArray(SomeClass2) someArrayOfObject: SomeClass2[];
+   @NestedArray(SomeClass2, {optional: true}) someArrayOfObjectOptional?: SomeClass2[];
+   constructor(device: Device) {
+  }
+}
 `
 
     const result = linter.verifyAndFix(code, {
--- fastest-validator-lint/src/lint.ts
+++ fastest-validator-lint/src/lint.ts
@@ -1,5 +1,5 @@
 import { ESLintUtils } from "@typescript-eslint/experimental-utils";
-import { AST_NODE_TYPES } from "@typescript-eslint/typescript-estree";
+import {AST_NODE_TYPES, TSESTree} from "@typescript-eslint/typescript-estree";
 
 
 export const createRule = ESLintUtils.RuleCreator(
@@ -46,10 +46,9 @@ export const rule =  createRule({
     ],
   },
   defaultOptions: [],
-  create (context)  {
-
+  create (context): any  {
     const handleType = (node: any, decoratorName: string, type?: string) =>{
-      const prop = node?.parent;
+      const prop = node;
       const hasBooleanDecorator = prop?.decorators && prop?.decorators.some((d: any) => d.expression.callee.name === decoratorName);
       if (hasBooleanDecorator){
         return;
@@ -76,19 +75,22 @@ export const rule =  createRule({
           node: node,
           messageId: "errorMissingTypeValidation",
           fix (fixer) {
-            return [fixer.insertTextBefore(node,"@Schema()\n")];
+            return [fixer.insertTextBefore(node.parent?.type === "ExportNamedDeclaration" ? node.parent as TSESTree.Node: node,
+              "@Schema()\n")];
           },
         });
       },
-      TSTypeAnnotation (node){
-        if (node.typeAnnotation.type === "TSTypeReference")
+      ClassProperty (node): void{
+        const type = node.typeAnnotation?.typeAnnotation?.type;
+        if (!type) return;
+        if (type === "TSTypeReference")
           handleType(node, "Nested");
-        else if (node.typeAnnotation.type === "TSArrayType"){
-          const t = (node as any).typeAnnotation.elementType?.typeName?.name;
+        else if (type === "TSArrayType"){
+          const t = node.typeAnnotation?.typeAnnotation?.elementType?.typeName?.name;
           handleType( node, t ? "NestedArray" : "Array", t);
         }
-        else if (isPrimitiveType(node.typeAnnotation.type))
-          handleType(node, node.typeAnnotation.type.replace("TS","").replace("Keyword", ""));
+        else if (isPrimitiveType(type))
+          handleType(node, type.replace("TS","").replace("Keyword", ""));
       }
     };
   },
